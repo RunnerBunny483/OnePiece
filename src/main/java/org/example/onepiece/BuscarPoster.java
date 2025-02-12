7b6085a1f47aef62fa09b49056c9726a745c77a9
runnerbunny483 <runnerbunny483@gmail.com>
2025-02-12 12:14:42 +0100

    Cambio en el toggle group

--- src/main/java/org/example/onepiece/BuscarPoster.java
+++ src/main/java/org/example/onepiece/BuscarPoster.java
@@ -1,4 +1,14 @@
 package org.example.onepiece;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class BuscarPoster {
+    @FXML
+    private ComboBox<String> desplegablePirata;
+
+    public void initialize() {
+        //Añadir los idiomas al combo box
+        desplegablePirata.getItems().addAll("Español", "English");
+
 }
