41c0f8cf88497ed1e314a0297b4f7fc30e0b6abd
xpanchamcode <marcospjob.5@gmail.com>
2025-02-13 18:07:38 +0100

    Push generarInforme

--- src/main/java/org/example/onepiece/GenerarInforme.java
+++ src/main/java/org/example/onepiece/GenerarInforme.java
@@ -5,21 +5,24 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 
-import java.awt.*;
+import java.io.File;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.util.*;
+import java.util.List;
 
 /**
  * Controlador de la vista generar-indorme.fxml para la ventana de generación de informes según los parámetros introducidos por el usuario.
  * Permite al usuario introducir los datos que desee en los campos Tipulación y recompensa.
 
- * @author Gabriela Jiménez Morcillo
+ * @author Gabriela Jiménez Morcillo, Marcos Pérez Aceituno
  */
 public class GenerarInforme {
 
@@ -34,7 +37,7 @@ public class GenerarInforme {
     @FXML
     private Label labelTripulacion, labelRecompensa;
 
-    private ArrayList<String> listaTripulaciones;
+    private List<String> listaTripulaciones;
 
     private Connection conexion;
 
@@ -47,20 +50,14 @@ public class GenerarInforme {
 
         ResourceBundle resourceBundle= IdiomaController.getResourceBundle();
         String tripulacionComboBox = resourceBundle.getString("ComboBoxTripulacion");
-//        listaTripulaciones= tripulacionComboBox.split(",");
+        //Array de Strings a List<String>
+        listaTripulaciones= Arrays.asList(tripulacionComboBox.split(","));
 
         Tripulacion.getItems().clear();
         for (String tripulacion : listaTripulaciones) {
             Tripulacion.getItems().add(tripulacion);
         }
     }
-    //cuando se haga una consulta en inglés pillar la posición de la tripula
-    //If locale eng -->
-    //Gettear posicion en el array de la tripulacion
-    //Cambiar locale a esp, gettear listaTripulaciones otra vez
-    //Gettear posicion en eng en la lista en esp
-    //Guardarlo en variable que se envía al jasper
-    //Cambiar locale a eng otra vez
 
     /**
      * Actualiza los textos de la ventana según el idioma seleccionado.
@@ -75,6 +72,10 @@ public class GenerarInforme {
 
     }
 
+    /**
+     * Genera un informe a raíz de los parámetros de búsqueda elegidos por el usuario,
+     * en la ruta elegida por el mismo
+     */
     public void generarInforme(){
         conexion = Conexion.getConexion();
 
@@ -83,9 +84,9 @@ public class GenerarInforme {
         String tripulacion = Tripulacion.getValue().trim();
 
         if (!recompensaMinima.isEmpty()) {
-            try{
+            try {
                 parametros.put("BerriesMax", Long.parseLong(recompensaMinima));
-            } catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 throw new NumberFormatException("Recompensa mínima inválida.");
             }
         } else {
@@ -93,59 +94,57 @@ public class GenerarInforme {
         }
 
         if (!tripulacion.isEmpty()) {
-            if(IdiomaController.getLocale().getLanguage().equals("en")) {
-                listaTripulaciones
+            //Si el idioma está en inglés:
+            if (IdiomaController.getLocale().getLanguage().equals("en")) {
+                //Guardamos la posicion en la lista de la tripulación en inglés
+                int posicion = listaTripulaciones.indexOf(tripulacion);
+                //Convertimos el idioma a español para enviar el dato en el idioma original
+                IdiomaController.seleccionIdioma("Español");
+                String tripulacionComboBox = IdiomaController.getResourceBundle().getString("ComboBoxTripulacion");
+                List<String> listaTripulacionesEsp = Arrays.asList(tripulacionComboBox.split(","));
+                parametros.put("Tripulacion", listaTripulacionesEsp.get(posicion));
+
+                //Reseteamos el idioma a español
+                IdiomaController.seleccionIdioma("English");
+            } else {
+                parametros.put("Tripulacion", tripulacion);
             }
-            parametros.put("Distrito", distrito);
         } else {
-            parametros.put("Distrito", null);
+            parametros.put("Tripulacion", null);
         }
-
-        if (!bando.isEmpty()) {
-            parametros.put("Bando", bando);
-        } else {
-            parametros.put("Bando", null);
-        }
-
         System.out.println("Parámetros seleccionados: " + parametros);
 
-        informeParametros(parametros);
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Guardar Informe de Jugadores");
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Archivos PDF", "*.pdf"));
-        fileChooser.setInitialFileName("Informe_Jugadores.pdf");
-
-        // Mostrar el cuadro de diálogo para guardar el archivo
-        File file = fileChooser.showSaveDialog(new Stage());
-
-        // Verificar si el usuario ha seleccionado un archivo
-        if (file != null) {
-            String reportPath = "src/main/recursos/LaLiga.jasper";  // Ruta directa
-            System.out.println("Ruta del informe: " + reportPath);
-
-            JasperPrint print = JasperFillManager.fillReport(reportPath, parametros, conexion);
-            JasperExportManager.exportReportToPdfFile(print, file.getAbsolutePath());
-
-            System.out.println("Informe generado correctamente en: " + file.getAbsolutePath());
-        }
-
-    } catch (ClassNotFoundException | JRException e) {
-        System.out.println("Error al generar el informe de jugadores: " + e.getMessage());
-    }
+        //Llamada al método de imprimir informe
+        imprimirInforme("/OnePieceInforme-2Params.jasper", parametros);
 }
 
-    private void informeParametros(Map<String, Object> parametros) throws JRException {
-        InputStream jasperStream = getClass().getResourceAsStream("/DetectiveConan.jasper");
-        JasperPrint print = JasperFillManager.fillReport(jasperStream, parametros, conexion);
-        JasperExportManager.exportReportToPdfFile(print, rutaDirectorioSeleccionado+"/informeConan.pdf");
-    }
+    /**
+     * Impresión del informe y elección de su ruta de creación
+     * @param ficheroInforme ruta del jasper en el proyecto Java
+     * @param parametros parámetros seleccionados para la creación del informe
+     */
+    private void imprimirInforme(String ficheroInforme, Map<String, Object> parametros){
+        try{
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Guardar el Informe de los Piratas");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Archivos PDF", "*.pdf"));
+            fileChooser.setInitialFileName("Informe_Piratas.pdf");
 
-    private void informeGroup() throws JRException{
-        Map<String, Object> paramVacios = new HashMap<>();
+            //Se muestra el fileChooser
+            File rutaInforme = fileChooser.showSaveDialog(new Stage());
 
-        InputStream jasperStream = getClass().getResourceAsStream("/DetectiveConanGroup.jasper");
-        JasperPrint print = JasperFillManager.fillReport(jasperStream, paramVacios, conexion);
+            //Se verifica si el usuario ha seleccionado un archivo
+            if (rutaInforme != null) {
+                String rutaJasper = "src/main/resources" + ficheroInforme;
+                System.out.println("Ruta del informe elegido: " + rutaJasper);
 
-        JasperExportManager.exportReportToPdfFile(print, rutaDirectorioSeleccionado+"/informeConanGroup.pdf");
+                JasperPrint print = JasperFillManager.fillReport(rutaJasper, parametros, conexion);
+                JasperExportManager.exportReportToPdfFile(print, rutaInforme.getAbsolutePath());
+
+                System.out.println("Informe generado correctamente en: " + rutaInforme.getAbsolutePath());
+            }
+        } catch (JRException ex) {
+            System.out.println("Error al generar el informe: " + ex.getMessage());
+        }
     }
 }
